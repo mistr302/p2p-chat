2fd85ea47a0ed9d8426b7fd6b372cdf300319a6b
mistr302 <michal.stransky42@gmail.com>
2026-03-24 15:39:19 +0100

    connection in mem only for now

--- p2pchat-core/src/main.rs
+++ p2pchat-core/src/main.rs
@@ -22,14 +22,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
     create_project_dirs().unwrap();
 
     // TODO: again remove ARC got fucked
+    // let sqlite = Arc::new(
+    //     tokio_rusqlite::Connection::open(get_save_file_path(SaveFile::Database))
+    //         .await
+    //         .expect("Couldnt open sqlite connection"),
+    // );
     let sqlite = Arc::new(
-        tokio_rusqlite::Connection::open(get_save_file_path(SaveFile::Database))
+        tokio_rusqlite::Connection::open_in_memory()
             .await
             .expect("Couldnt open sqlite connection"),
     );
-    // let sqlite = tokio_rusqlite::Connection::open_in_memory()
-    //     .await
-    //     .expect("Couldnt open sqlite connection");
 
     // Open unix socket
     // TODO: handle if sock exists
@@ -79,6 +81,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
             }
             req = read_event(&mut sock_read) => {
                 let req = req?;
+                tracing::info!("Received event: {:?}", req);
                 let id = req.req_id;
                 match req.event {
                     UiClientEvent::Close => {
